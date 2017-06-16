593440306d22048262d17a96c77f96e69cb4ef2f
jimscarver <jimscarver@gmail.com>
2017-06-16 18:34:42 -0400

    Scala IDE files and expose scripts added

---
+++ rbl/rosette/rosette.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import request
+from subprocess import check_output
+app = Flask(__name__)
+ 
+@app.route("/", methods=['GET', 'POST'])
+def rosette():
+    return check_output(["/bin/bash","ros", request.form['text']])
+ 
+if __name__ == "__main__":
+    app.run()
